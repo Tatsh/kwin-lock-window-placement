e5fe96e7a63f88afc881c80cd6abf8fbe4c2a54a
Andrew Udvare <audvare@gmail.com>
2020-04-30 20:44:19 -0400

    contents/code/main: update

--- contents/code/main.js
+++ contents/code/main.js
@@ -1,40 +1,26 @@
-registerUserActionsMenu(function () {
-  var callbacks = {},
-    forceGeometryCallback = function (geometry) {
-      return function (client) {
+"use strict";
+registerUserActionsMenu((function () {
+    var callbacks = {};
+    var forceGeometryCallback = function (geometry) { return function (client) {
         client.geometry = geometry;
-      };
-    };
-  return function (client) {
-    return {
-      checkable: true,
-      checked: !!callbacks[client.windowId],
-      text: 'Locked',
-      triggered: function (action) {
-        if (action.checked) {
-          callbacks[client.windowId] = forceGeometryCallback(client.geometry);
-          client.clientStartUserMovedResized.connect(
-            callbacks[client.windowId],
-          );
-          client.clientStepUserMovedResized.connect(
-            callbacks[client.windowId],
-          );
-          client.clientFinishUserMovedResized.connect(
-            callbacks[client.windowId],
-          );
-        } else {
-          client.clientStartUserMovedResized.disconnect(
-            callbacks[client.windowId],
-          );
-          client.clientStepUserMovedResized.disconnect(
-            callbacks[client.windowId],
-          );
-          client.clientFinishUserMovedResized.disconnect(
-            callbacks[client.windowId],
-          );
-          delete callbacks[client.windowId];
-        }
-      },
-    };
-  };
-}());
+    }; };
+    return function (client) { return ({
+        checkable: true,
+        checked: !!callbacks[client.windowId],
+        text: 'Locked',
+        triggered: function (action) {
+            if (action.checked) {
+                callbacks[client.windowId] = forceGeometryCallback(client.geometry);
+                client.clientStartUserMovedResized.connect(callbacks[client.windowId]);
+                client.clientStepUserMovedResized.connect(callbacks[client.windowId]);
+                client.clientFinishUserMovedResized.connect(callbacks[client.windowId]);
+            }
+            else {
+                client.clientStartUserMovedResized.disconnect(callbacks[client.windowId]);
+                client.clientStepUserMovedResized.disconnect(callbacks[client.windowId]);
+                client.clientFinishUserMovedResized.disconnect(callbacks[client.windowId]);
+                delete callbacks[client.windowId];
+            }
+        },
+    }); };
+})());
